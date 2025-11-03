1e765c5c98223e3a8b25def2db5ae29fb08af5ac
Lokesh Gunaganti <Lokesh.Gunaganti@encora.com>
2025-11-03 16:40:55 +0530

    included states

--- userprofile/src/App.js
+++ userprofile/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Header from './Header';
-import Profile from './Profile';
+import Profile from './profile';
 import Footer from './Footer';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name,setName]=useState("")
   return (
     <div className="App">
-      <Header Name="Lokesh" />
+      <Header Name={name} />
+      
+
       <div className="Profile">
+        <input type="text"
+      placeholder='Enter Name'
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      ></input>
         <Profile
-          name="Lokesh"
-          email="lokesh@example.com"
+          name={name}
           role="Software Consultant"
           location="Bangalore, India"
         />
